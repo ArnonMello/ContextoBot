4b37c4c6bc37898da2fd9fea1d41c064416f8bee
Arnon <arnonmellob4@hotmail.com>
2022-07-26 11:59:20 -0300

    Better formating

--- crawlerBot.py
+++ crawlerBot.py
@@ -10,40 +10,48 @@ from selenium.webdriver.common.keys import Keys
 PATH = "C:\chromedriver.exe"
 nlp = spacy.load("pt_core_news_lg")
 
+INITIAL_QTY_WORDS_TO_GENERATE = 20
+INITIAL_CURRENT_SCORE = 20
+
 randomWords = ["cor", "flor", "fruta", "luz", "vida", "família", "computador", "memória", "sol", "rocha", "mente", "aceitar", "ganhar", "loucura", "pessoa", "cabelo", "matemática", "língua"]
 triedWords = set()
 guessedWords = set()
-qtyWords = 20
-currentScore = 100
+qtyWordsToGenerate = INITIAL_QTY_WORDS_TO_GENERATE
+currentScore = INITIAL_CURRENT_SCORE
+
+xPathOptions = '//*[@id="root"]/div/div[1]/div[2]/button'
+xPathPastGames = '//*[@id="root"]/div/div[1]/div[2]/div[2]/button[4]'
+xPathInput = '//*[@id="root"]/div/form/input'
+
+def getXPathPastGame(number):
+  return str('//*[@id="root"]/div/div[5]/div/div[2]/div/div/div[{}]/button').format(number)
 
 driver = webdriver.Chrome(PATH)
 driver.get('https://contexto.me/')
 time.sleep(2)
 
+def clickAndWaitXPath(xPath):
+  element = driver.find_element(By.XPATH, xPath)
+  time.sleep(0.2)
+  element.click()
+  time.sleep(0.2)
+  return element
+
 def setup():
-  options = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[1]/div[2]/button')
-  time.sleep(0.3)
-  options.click()
-  time.sleep(0.3)
-  pastGames = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[1]/div[2]/div[2]/button[4]')
-  time.sleep(0.3)
-  pastGames.click()
-  time.sleep(0.3)
-  oldGame = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[5]/div/div[2]/div/div/div[4]/button')
-  time.sleep(0.3)
-  oldGame.click()
-  time.sleep(0.3)
-  inputElement = driver.find_element(By.XPATH, '//*[@id="root"]/div/form/input')
+  clickAndWaitXPath(xPathOptions)
+  clickAndWaitXPath(xPathPastGames)
+  clickAndWaitXPath(getXPathPastGame(1))
+  inputElement = driver.find_element(By.XPATH, xPathInput)
   time.sleep(2)
   tryGuessWords(inputElement, randomWords)
   return inputElement
 
 
 def getSimilarWords(word):
-  if(qtyWords == 0):
+  if(qtyWordsToGenerate == 0):
     return []
   ms = nlp.vocab.vectors.most_similar(
-    np.asarray([nlp.vocab.vectors[nlp.vocab.strings[word]]]), n=qtyWords)
+    np.asarray([nlp.vocab.vectors[nlp.vocab.strings[word]]]), n=qtyWordsToGenerate)
   words = [nlp.vocab.strings[w] for w in ms[0][0]]
   words = [w for w in words if not search('-', str(w))]
   return words
@@ -58,8 +66,8 @@ def getNextWord():
       rowData = driver.find_element(By.XPATH, xPath).text
       wordInRow = rowData[:rowData.find("\n")]
       rowScore = rowData[rowData.find("\n"):]
-      global qtyWords
-      qtyWords = int(10*min(20, int(1000/int(rowScore))))
+      global qtyWordsToGenerate
+      qtyWordsToGenerate = int(10*min(20, int(1000/int(rowScore))))
       if wordInRow not in triedWords:
         global currentScore
         currentScore = int(rowScore)
@@ -70,14 +78,17 @@ def getNextWord():
 
 def tryGuessWords(inputElement, wordList):
   for guessWord in wordList:
+
       if(guessWord in guessedWords):
         continue
+      
       inputElement.clear()
       time.sleep(0.1)
       inputElement.send_keys(guessWord)
       time.sleep(0.1)
       inputElement.send_keys(Keys.ENTER)
       time.sleep(0.4)
+
       try:
         guessedScore = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[3]/div/div[2]/span[2]').text
         #//*[@id="root"]/div/div[3]/div/div[2]/span[2]
@@ -89,7 +100,7 @@ def tryGuessWords(inputElement, wordList):
           tryGuessWords(inputElement, wordListNew)
           triedWords.add(guessWord)
           currentScore = pastScore
-      except Exception as e:
+      except Exception:
         time.sleep(0.1)
 
     
