9f691427d498937dff23dc7860242e25d13e5463
Arnon <arnonmellob4@hotmail.com>
2022-07-26 22:32:14 -0300

    Using priority queue to select next word

--- crawlerBot.py
+++ crawlerBot.py
@@ -1,3 +1,4 @@
+import queue
 import time
 from re import search
 
@@ -7,109 +8,142 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-PATH = "C:\chromedriver.exe"
 nlp = spacy.load("pt_core_news_lg")
 
 INITIAL_QTY_WORDS_TO_GENERATE = 20
-INITIAL_CURRENT_SCORE = 20
+INITIAL_SCORE = 500
+NUMBER_PAST_GAME = 5
+DRIVER_PATH = r"C:\chromedriver.exe"
 
-randomWords = ["cor", "flor", "fruta", "luz", "vida", "família", "computador", "memória", "sol", "rocha", "mente", "aceitar", "ganhar", "loucura", "pessoa", "cabelo", "matemática", "língua"]
-triedWords = set()
+INITIAL_WORDS = ["cor", "flor", "fruta", "luz", "vida", "família", "computador", "memória", "sol", "rocha", "mente", "aceitar", "ganhar", "loucura", "pessoa", "cabelo", "matemática", "língua"]
+
+processedWords = set()
 guessedWords = set()
+wordsToProcess = queue.PriorityQueue()
+
 qtyWordsToGenerate = INITIAL_QTY_WORDS_TO_GENERATE
-currentScore = INITIAL_CURRENT_SCORE
+currentScore = INITIAL_SCORE
 
 xPathOptions = '//*[@id="root"]/div/div[1]/div[2]/button'
 xPathPastGames = '//*[@id="root"]/div/div[1]/div[2]/div[2]/button[4]'
 xPathInput = '//*[@id="root"]/div/form/input'
+xPathGuessedScore = '//*[@id="root"]/div/div[3]/div/div[2]/span[2]'
+xPathTentatives = '//*[@id="root"]/div/div[2]/span[4]'
+
+global driver
+global inputElement
+
+
+
+class WordInfo:
+  def __init__(self, word, score):
+    self.word = word
+    self.score = score
+  
+  def __lt__(self, other):
+    if self.score == other.score:
+      return self.word < other.word
+
+    return self.score < other.score
 
 def getXPathPastGame(number):
   return str('//*[@id="root"]/div/div[5]/div/div[2]/div/div/div[{}]/button').format(number)
 
-driver = webdriver.Chrome(PATH)
-driver.get('https://contexto.me/')
-time.sleep(2)
 
 def clickAndWaitXPath(xPath):
+  global driver
   element = driver.find_element(By.XPATH, xPath)
   time.sleep(0.2)
   element.click()
   time.sleep(0.2)
   return element
 
-def setup():
+
+def addWordToProcess(wordInfo):
+  if wordInfo.word in processedWords:
+    return
+  
+  wordsToProcess.put(wordInfo)
+
+
+def getInputAndSetup():
+  global driver
+  global inputElement
+
+  driver = webdriver.Chrome(DRIVER_PATH)
+  driver.get('https://contexto.me/')
+  time.sleep(2)
+
   clickAndWaitXPath(xPathOptions)
   clickAndWaitXPath(xPathPastGames)
-  clickAndWaitXPath(getXPathPastGame(1))
+  clickAndWaitXPath(getXPathPastGame(NUMBER_PAST_GAME))
   inputElement = driver.find_element(By.XPATH, xPathInput)
   time.sleep(2)
-  tryGuessWords(inputElement, randomWords)
+
+  for word in INITIAL_WORDS:
+    score = getWordScore(word)
+    wordInfo = WordInfo(word, score)
+    wordsToProcess.put(wordInfo)
+
   return inputElement
 
 
 def getSimilarWords(word):
   if(qtyWordsToGenerate == 0):
     return []
-  ms = nlp.vocab.vectors.most_similar(
-    np.asarray([nlp.vocab.vectors[nlp.vocab.strings[word]]]), n=qtyWordsToGenerate)
-  words = [nlp.vocab.strings[w] for w in ms[0][0]]
+  ms = nlp.vocab.vectors.most_similar(np.asarray([nlp.vocab.vectors[nlp.vocab.strings[word]]]), n=qtyWordsToGenerate)
+  words = [nlp.vocab.strings[w] for w in ms [0][0]]
   words = [w for w in words if not search('-', str(w))]
   return words
 
 
-def getNextWord():
-  tentatives = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/span[4]')
-  tentatives = int(tentatives.text)
+def getWordScore(guessWord):
+  if(guessWord in guessedWords):
+    return
+  
+  global inputElement
+  inputElement.clear()
+  time.sleep(0.1)
+  inputElement.send_keys(guessWord)
+  time.sleep(0.1)
+  inputElement.send_keys(Keys.ENTER)
+  time.sleep(0.4)
+  guessedWords.add(guessWord)
+
   try:
-    for row in range(1, tentatives+1):
-      xPath = '//*[@id="root"]/div/div[4]/div[{}]/div[2]'.format(row)
-      rowData = driver.find_element(By.XPATH, xPath).text
-      wordInRow = rowData[:rowData.find("\n")]
-      rowScore = rowData[rowData.find("\n"):]
-      global qtyWordsToGenerate
-      qtyWordsToGenerate = int(10*min(20, int(1000/int(rowScore))))
-      if wordInRow not in triedWords:
-        global currentScore
-        currentScore = int(rowScore)
-        triedWords.add(wordInRow)
-        return wordInRow
-  except Exception as e:
-    print(e)
+    guessedScore = int(driver.find_element(By.XPATH, xPathGuessedScore).text)
+    wordsToProcess.put(WordInfo(guessWord, guessedScore))
+    return guessedScore
 
-def tryGuessWords(inputElement, wordList):
-  for guessWord in wordList:
+  except Exception:
+    time.sleep(0.1)
 
-      if(guessWord in guessedWords):
-        continue
-      
-      inputElement.clear()
-      time.sleep(0.1)
-      inputElement.send_keys(guessWord)
-      time.sleep(0.1)
-      inputElement.send_keys(Keys.ENTER)
-      time.sleep(0.4)
+  return False
 
-      try:
-        guessedScore = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[3]/div/div[2]/span[2]').text
-        #//*[@id="root"]/div/div[3]/div/div[2]/span[2]
-        global currentScore
-        if int(guessedScore) < currentScore:
-          pastScore = currentScore
-          currentScore = int(guessedScore)
-          wordListNew = getSimilarWords(guessWord)
-          tryGuessWords(inputElement, wordListNew)
-          triedWords.add(guessWord)
-          currentScore = pastScore
-      except Exception:
-        time.sleep(0.1)
+
+def generateAndGuessSimilarWords(wordInfo):
+  if wordInfo.word in processedWords:
+    return
+  
+  similarWords = getSimilarWords(wordInfo.word)
+  processedWords.add(wordInfo.word)
+
+  for similarWord in similarWords:
+    getWordScore(similarWord)
+
+    #note: At this point it is possible to have a new word with score less than wordInfo.score, so lets check it at priorityQueue
+    newWordInfo = wordsToProcess.queue[0]
+    if(newWordInfo.score < wordInfo.score):
+      wordsToProcess.get()
+      generateAndGuessSimilarWords(newWordInfo)
 
     
-def main(): 
-  inputElement = setup()
+def main():   
+  getInputAndSetup()
+
   while True:
-    word = getNextWord()
-    wordList = getSimilarWords(word)
-    tryGuessWords(inputElement, wordList)
+    wordInfo = wordsToProcess.get()
+    generateAndGuessSimilarWords(wordInfo)
 
   time.sleep(1000)
 
